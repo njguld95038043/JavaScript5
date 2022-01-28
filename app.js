518273194e08e3819827a36f72376c948aac589d
t t <tt@tinoMacBook-Air.local>
2022-01-28 11:43:18 +0900

    [Finish]

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
-let i;
-let num = 0;
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-for(i = 1;i < 11;i++){
-  num = num + i;
+let js_hand = getJShand();
+
+let judge = winLose(user_hand,js_hand);
+
+alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  } else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  } else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+  return js_hand;
 }
-alert('1から10まで足し算した結果は' + num + 'です');
+
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
